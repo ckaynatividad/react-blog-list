83dce45323b50540bc9be9370b67dbd4be967192
ckaynatividad <natividad.ckay@gmail.com>
2021-12-06 15:32:33 -0800

    calling blogs using usestate and making getBlogs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { getBlogs } from './services/blogs';
+import BlogCard from './components/BlogCard/BlogCard';
 import Header from './components/layout/Header/Header';
 import Footer from './components/layout/Footer/Footer';
 
 function App() {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getBlogs();
+      setBlogs(data);
+    };
+    fetchData();
+  }, []);
   return (
     <section className="main">
       <Header />
+      {blogs.map((blog) => {
+        <BlogCard {...blog} key={blog.id} />;
+      })}
       <Footer />
     </section>
   );
